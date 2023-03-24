b8e384df4863051c9130c403cc27d78f1888517f
brightdaniel <ivquestservices@gmail.com>
2023-03-24 15:15:40 +0100

    home_page

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('index.html')
+
+@app.route('/mission')
+def mission_page():
+    return render_template('mission.html')
