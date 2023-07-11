854e11edb5d4736f003c5548261a6e05d8f273f6
bright daniel <ivquestservices@gmail.com>
2023-07-11 19:49:24 +0100

    configuration for heroku hosting

--- app.py
+++ app.py
@@ -102,20 +102,7 @@ class Admin(db.Model):
         return bcrypt.check_password_hash(self.password, password)
 
 
-# class Course(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(255), nullable=False)
-#     cost = db.Column(db.Float, nullable=False)
-#     eligibility = db.Column(db.String(255), nullable=False)
-#     duration = db.Column(db.Integer, nullable=False)
-#     slug = db.Column(db.String(255), unique=True, nullable=False)
-#     cover_photo = db.Column(db.String(255), nullable=False)
-#     course_content = db.Column(db.Text, nullable=False)
-#     start_date = db.Column(db.Date, nullable=True)
-#     deadline_date = db.Column(db.Date, nullable=True)
-#     quizzes = db.relationship('Quiz', backref='course', lazy=True)
-#     applications = db.relationship('Application', backref='course', lazy='dynamic')
-    
+
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
@@ -160,24 +147,6 @@ class Blog(db.Model):
     
 
 
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(255), nullable=False)
-#     last_name = db.Column(db.String(255), nullable=False)
-#     email = db.Column(db.String(255), unique=True, nullable=False)
-#     password_hash = db.Column(db.String(255), nullable=False)
-#     country = db.Column(db.String(255), nullable=False)
-#     dob = db.Column(db.Date, nullable=False)
-#     gender = db.Column(db.String(10), nullable=False)
-#     registered_courses = db.relationship('Course', secondary=registration_table, backref=db.backref('registered_users', lazy='dynamic'))
-
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
-
-#     def check_password(self, password):
-#         return check_password_hash(self.password_hash, password)
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255), nullable=False)
@@ -195,19 +164,6 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-# class Application(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
-#     phone = db.Column(db.String(20), nullable=False)
-#     country = db.Column(db.String(100), nullable=False)
-#     state = db.Column(db.String(100), nullable=False)
-#     education = db.Column(db.String(100), nullable=False)
-#     skills = db.Column(db.String(100), nullable=False)
-#     time = db.Column(db.Integer, nullable=False)
-#     motivation = db.Column(db.Text, nullable=False)
-#     user = db.relationship('User', backref='applications')
-#     course = db.relationship('Course', backref='applications')
 
 class Application(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -390,7 +346,7 @@ def admin_login_page():
 @app.route('/admin-dashboard')
 def admin_dashboard():
     if 'admin' in session:
-        email = admin_email  
+        email = admin_email   
         return render_template('admin_dashboard.html', email=email)
     else:
         return redirect('/admin-login')
